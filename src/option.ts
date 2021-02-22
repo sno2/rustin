c61a23366fc33aa0707b6ad252daace8de640a7b
Leon Davis <bodged@pm.me>
2021-02-22 23:14:48 +0000

    patch: add examples to option

--- src/option.ts
+++ src/option.ts
@@ -1,22 +1,76 @@
-/** Represents some value. */
+/**
+ * Represents some value.
+ * 
+ * ## Example
+ * ```
+ * const some: Some<number> = 50;
+ * ```
+ */
 export type Some<T> = T;
-/** Represents no value. */
+/**
+ * Represents no value.
+ * 
+ * ## Example
+ * ```
+ * const none: None = null;
+ * ```
+ */
 export type None = null | undefined;
-/** Represents either some value (`Some`) or no value (`None`). */
+/**
+ * Represents either some value (`Some`) or no value (`None`).
+ * 
+ * ## Examples
+ * `Some` (some value):
+ * ```
+ * const some: Some<number> = 50;
+ * ```
+ * `None` (no value):
+ * ```
+ * const none: None = null;
+ * ```
+ */
 export type Option<T> = Some<T> | None;
 
-/** Returns `true` if the option is a `Some` value. */
+/**
+ * Returns `true` if the option is a `Some` value.
+ * 
+ * ## Example
+ * ```
+ * const some: Some<number> = 50;
+ * console.log(option.isSome(some)); // true
+ * ```
+ */
 export function isSome<T>(self: Option<T>): boolean {
   if(typeof(self) !== "undefined" && self !== null) return true;
   return false;
 }
 
-/** Returns `true` if the option is a `None` value. */
+/**
+ * Returns `true` if the option is a `None` value.
+ * 
+ * ## Example
+ * ```
+ * const none: None = null;
+ * console.log(option.isNone(none)); // true
+ * ```
+ */
 export function isNone<T>(self: Option<T>): boolean {
   return !isSome(self);
 }
 
-/** Returns `true` if the option is a `Some` value containing the given value. */
+/**
+ * Returns `true` if the option is a `Some` value containing the given value.
+ * 
+ * ## Example
+ * ```
+ * const some: Some<number> = 50;
+ * const none: None = null;
+ * 
+ * console.log(option.contains(some, 50)); // true
+ * console.log(option.contains(some, 100)); // false
+ * console.log(option.contains(none, 50); // false
+ * ```
+ */
 export function contains<T>(self: Option<T>, value: T): boolean {
   if(isSome(self) && self === value) return true;
   return false;
@@ -28,8 +82,17 @@ export function contains<T>(self: Option<T>, value: T): boolean {
  * Because this function may panic, its use is generally discouraged. Instead,
  * prefer to handle the `None` case explicitly.
  * 
- * # Panics
- * Panics if the value equals `None`.
+ * ## Throws
+ * Throws if the value equals `None`.
+ * 
+ * ## Example
+ * ```
+ * const some: Some<number> = 50;
+ * console.log(option.unwrap(some)); // 50
+ * 
+ * const none: None = null;
+ * console.log(option.unwrap(none)); // throws
+ * ```
  */
 export function unwrap<T>(self: Option<T>): T {
   if(isSome(self)) return self as Some<T>;
@@ -41,8 +104,17 @@ export function unwrap<T>(self: Option<T>): T {
 /**
  * Returns the contained `Some` value.
  * 
- * # Panics
- * Panics if the value is a `None` with a custom panic message provided by `msg`.
+ * ## Throws
+ * Throws if the value is a `None` with a custom panic message provided by `msg`.
+ * 
+ * ## Example
+ * ```
+ const some: Some<number> = 50;
+ * console.log(option.expect(some, "whoops")); // 50
+ * 
+ * const none: None = null;
+ * console.log(option.expect(none, "whoops")); // throws with custom message
+ * ```
  */
 export function expect<T>(self: Option<T>, msg: string): T {
   if(isSome(self)) return self as Some<T>;
